fb67177c3ba247f949ef1c4ebf6afe551a54e01f
Matt Panzer <panzer.matthew@gmail.com>
2017-10-13 15:15:43 -0400

    wrote script for turning into greyscale. there are some intermediate steps here where you have an array of color values.
    you'll need pip, numpy and PIL (or Pillow)

--- object-detect.py
+++ object-detect.py
@@ -1,2 +1,34 @@
+import sys
+from PIL import Image
+from numpy import*
 
-this is the python file
+def brightness(color):
+    b = color[0] * 0.3 + color[1] * 0.59 + color[2] * 0.11
+    return int(b)
+
+def toGreyscale(color):
+    b = brightness(color);
+    return repeat(b, 3)
+
+file = input('File name (with ext): ')
+
+im = Image.open('images/' + file)
+WIDTH, HEIGHT = im.size
+
+a = asarray(im)
+
+flat_length = WIDTH * HEIGHT
+flat_shape = (flat_length, 3)
+twod_shape = (HEIGHT, WIDTH, 3)
+a_flat = a.reshape(flat_shape);
+
+r = zeros(flat_shape)
+for i in range(flat_length):
+    # do something on each pixel
+    pix = a_flat[i]
+    r[i] = toGreyscale(pix)
+
+r = r.reshape(twod_shape)
+print(r.shape)
+im = Image.fromarray(r.astype('uint8'))
+im.save("images/result001", "jpeg")
